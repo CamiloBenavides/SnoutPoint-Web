caaf399204b376ad35b217268104d7eb95128bf7
CamiloBenavides <p-etta@hotmail.com>
2015-04-21 17:17:55 +0000

    Revert "borrado de archivos inutiles"
    This reverts commit afdfbaa1c91bf4e499fcd30954795dfec961ee70.

---
+++ app/models/user_se.rb
@@ -0,0 +1,15 @@
+class UserSe < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+    validates_presence_of :nombre, :message => '^ El nombre del usuario no puede estar en blanco'
+    validates_presence_of :apellido, :message => '^ El apellido del usuario no puede estar en blanco'
+    validates_presence_of :fechaNacimiento ,:message => '^ La fecha del usuario no puede estar en blanco'
+  
+    validates_length_of :nombre, :maximum => 30
+    validates_length_of :apellido, :maximum => 30
+    
+    has_attached_file :image, :default_url => 't3.jpg', styles: { medium: '200x200>', thumb: '48x48>' } 
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+end
